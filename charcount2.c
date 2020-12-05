928de0a1def4656e1cc53bd0c81809db706e473d
Enziaki <jhashrijit@protonmail.com>
2020-12-05 06:55:31 +0530

    started a neew tutorial

--- charcount2.c
+++ charcount2.c
@@ -6,3 +6,5 @@ double main() {
 		;
 	printf("%0.f\n", nc);
 }
+/* One thing about while and for loops is that they test at the top
+ of the body of the loop before the body, which means it skips the body if there is nothing to be done */
---
+++ charcountnet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main () {
+printf("        _   \n");
+printf("       / \\  \n");	
+printf("      /   \\ \n");	
+printf("     /     \\ \n");
+printf("    /       \\ \n");	
+printf("   /         \\ \n");
+printf("  (__       __)\n");
+printf("     |     |\n");
+printf("     |     |\n");
+printf("     |     |\n");
+printf("     |     |\n");
+printf("     |     |\n");
+printf("  (((       )))\n");
+printf("(((((       )))))\n");
+return 0;
+}
+
