e6afc879ac84b4653af826fba91e65c4aeb89a13
Enziaki <jhashrijit@protonmail.com>
2020-12-11 05:20:19 +0530

    on the new course

---
+++ variable.c
@@ -0,0 +1,13 @@
+/* Variables basically */
+
+#include <stdio.h>
+int main() {
+	char name[] = "shitdixk4000";
+	int age = 69;
+	printf("There was this really edgy memelord type of a dumbass named %s \n", name);
+	printf("He was the epitome of what you shouldn't be at the age %d \n", age);
+
+	printf("\n");
+	printf("So the data type and all that have a certain % value that prints them\n");
+	printf("char will be used as\n char[] = s (for string otherwise it's c)\n with int being used with\n int = d \n ");
+}
