6b5b9263c651505e8c1df8cdeec98b0b3cc25ae8
Enziaki <jhashrijit@protonmail.com>
2020-12-30 04:55:15 +0530

    Just making my commits 69 for the year

--- variable.c
+++ variable.c
@@ -8,6 +8,20 @@ int main() {
 	printf("He was the epitome of what you shouldn't be at the age %d \n", age);
 
 	printf("\n");
+	printf("\n");
 	printf("So the data type and all that have a certain % value that prints them\n");
-	printf("char will be used as\n char[] = s (for string otherwise it's c)\n with int being used with\n int = d \n ");
+	printf("\n");
+	printf("for char\n");
+	printf("char[] = s (when used for a string with [], otherwise it's c)\n");
+	printf("\n");
+	printf("for int\n");
+	printf("int = d\n");
+	printf("\n");
+	printf("for float\n");
+	printf("float = f\n");
+	printf("\n");
+	printf("for char (The value is written in single quotes like 'A')\n");
+       	printf("char = \n");
+	
+	
 }
