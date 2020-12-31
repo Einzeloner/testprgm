95beb3a63442858a0bff1d2fc59f7c3b7a723dcd
Enziaki <jhashrijit@protonmail.com>
2020-12-31 20:28:11 +0530

    new program

---
+++ python/pithon.py
@@ -0,0 +1,24 @@
+#   This whole program is used as a wiki and a practice program. And yeah you comment using #.
+print("                 I HAVE NO IDEA OF PYTHON\n")
+print("\nPrint syntax=\n ")
+print("\nprint('yourstuffhere')\n\n       prints a new statement with 'yourstuffhere'. ")
+print('       Can print in single as well as in double quotes inside the parenthesis.\n')
+
+print("\nThe amount of newline character that I use is someday gonna give me a seizure\n")
+print("Hold on. Instead of using print for every line can I just write whatever the 'By Azura!' I want and it will register it as the way i wrote it? Wait lemme try.\n newline spamm boiiiii\n\nnewline spam again \n\n\n\n\n\n\n\nOkay! Enough autism for the day.\nFreaking newline reeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\nOkay back on the topic.\nWait. Enter in a sentence that is inside the print command does not work. 'EoL WhIlE ScAnNiNg StRiNg LiTeRal.'")
+print("\nVariables - no syntax required\n")
+print("     Variables can be created by simply declaring the value\n\n     x = 4\n\n     Here 'x' is a variable with value 4.\n")
+print("Now an example with code (Line 14 in program)\n\nSyntax=\n      x =5\n      print(x)\n")
+print("Solution=\n")
+
+# Declaring variables in python
+
+x = 5
+print(x)
+
+#Program end
+print("\n Variables can also be stored as a data type by simply stating the data type while declaring the variable.\n\nSyntax=\n        x = str(3)\n        print(x)\n")
+print("\nThis will print 3 as a string character. (Line 23 in program)\n")
+
+# Declaring variable with what is known as casting(When you put data type while writing the variable)
+
