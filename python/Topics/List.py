f69290be56ab5ad6ddde74fd64155dddf9a00035
Enziaki <jhashrijit@protonmail.com>
2021-01-22 08:05:29 +0530

    New tutorial and a change of workflow

--- python/Topics/List.py
+++ python/Topics/List.py
@@ -20,6 +20,33 @@ print(a)
 
 #accessing items from list
 
-j = print("First element is {}", x)
-if j in a:
-     print("Yeah this is the first element ", j)
+print("Checking for a word like 'whatever' in the list.")
+
+if "whatever" in a:
+     print("The list does have whatever.")
+else:
+     print("No the list does not have whatever in it.")
+
+
+#Changing list items
+
+print("Gonna change the last element of your list with whatever/\n")
+a[-1] = "whatever/"
+print('\n', a)
+ #Now the first element
+a[0] = "whatever@first"
+print('Changing the first element as well \n', a)
+
+
+#inserting an element into lists
+
+a.insert(-1, "AddingNewWhatever@last")
+print("Adding something to the last ", a)
+
+
+#changing a range of elements in a list
+
+print("Changing the values of the second to fourth element of the list.\n")
+a[1:3] = ["Whatever second", "whatever third", "whatever fourth"]
+print("The new list is\n", a)
+
