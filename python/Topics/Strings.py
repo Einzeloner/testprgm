8548ed280936a5f879fa687646b99f6dda88a757
Enziaki <jhashrijit@protonmail.com>
2021-01-27 06:29:34 +0530

    Rewriting strings into a different program

---
+++ python/Topics/Strings.py
@@ -0,0 +1,35 @@
+# Starting of the string topic
+
+#Storing string inside a variable 
+x = "This is how you store a string for a variable." 
+
+#printing that stored string
+print(x)
+
+#Printing string with the print command 
+print("Quotations are needed.")
+
+#printing a variable with some predefined string 
+print(x + " \nAnd this is an added text")
+
+#printing everything in lower case
+print(x.lower()) #Prints the first T in lower case.
+
+
+#Printing entirely in upper case
+print(x.upper()) #prints everything in upper case
+
+#checking whether the string is upper or lower case
+# This will return a true or a false value.
+
+print(x.isupper())
+#It will say false because the string is not entirely in upper case.
+
+#lets print check if a new variable is completely upper cased
+y = "THIS IS ENTIRELY IN UPPER CASE!"
+print(y)
+print(y.isupper())  
+#This will print the variable and check whether this is upper case or not
+
+
+#same can be done with lower case characters
