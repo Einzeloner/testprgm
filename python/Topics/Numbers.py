f123a8f5c48f2cbe2f7bd1397af75cad6570242c
Enziaki <jhashrijit@protonmail.com>
2021-02-01 11:40:00 +0530

    Numbers as the new topic

---
+++ python/Topics/Numbers.py
@@ -0,0 +1,19 @@
+#Basics of using numbers
+
+#printing  a number
+print(2)
+print(2.0997867254748)
+print(-3.564547)
+
+#numbers and operators can be in the same print statement.
+print(3*6)
+print(3-6)
+print(3+6)
+print(3/6)
+
+#Multiple operators
+print(3*5+77)
+#The interperter goes from left to right while solving equations, to alter teh flow we use parenthesis
+print(3*(5+77))
+
+
