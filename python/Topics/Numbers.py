8f19bcdef792139943fccdbe416a12b2d3f48c96
Enziaki <jhashrijit@protonmail.com>
2021-02-02 07:38:39 +0530

    User input and max function

--- python/Topics/Numbers.py
+++ python/Topics/Numbers.py
@@ -36,3 +36,26 @@ print(type(y))
 
 #Ah.. Note -  If you put anything in quotations while declaring the variable, python is gonna
 # think that it is a string.
+
+#absolute value of integer function in python.
+z = -5
+print("The variable declared is " + str(z))
+print(str(z) + " Has an absolute value of " + str(abs(z)))
+#The abs() function is used for finding an absolute value. 
+
+# Trying it out for something with decimals
+a = -2.3334454353
+print("The variable declared is " + str(a) + "\n")
+print(str(a) + " Has an absolute value of " + str(abs(a)))
+#So absolute value is basically a mod function. like | -1 | = 1
+
+
+#The pow function.
+print("Printing the variable z with a power of 4 using the powe function.\n")
+print("Using the pow() function the value will be " + str(pow(z, 4)) + "\n")
+#This is basically equal to -5^4
+
+#Using the max function to determine which input or variable or number has more value.
+#taking user input using input
+num1 = int(input("Enter two numbers from which python will determine which one has more value.: "))
+num2 = int(input("Another number: "))
