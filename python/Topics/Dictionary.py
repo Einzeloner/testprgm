53db799ee4722db1ffc46a5de344d7fe07dbd336
Enziaki <jhashrijit@protonmail.com>
2021-02-09 06:39:04 +0530

    Dictionary and functions starting

---
+++ python/Topics/Dictionary.py
@@ -0,0 +1,11 @@
+#A simple python dictionary program.
+
+#A dictionary is enclosed in '{}' while a list is enclosed in '[]'
+
+
+#defining a dictionary
+x = {"FirstElement": "OneeChan", "SecondElement": "Hazukashii", "ThirdElement": "baka!", "FourthElement": ">_<"}
+
+#Printing a dictionary element with the name defined and the index
+print(str(x["FirstElement"]) + "\nCannot print with indexing in dictionaries lol.\n") 
+
