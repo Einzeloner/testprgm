d79c47572073edcc34526c2c16ba1dd838f6cbe3
Enziaki <jhashrijit@protonmail.com>
2021-02-17 14:00:23 +0530

    returning values

---
+++ python/Topics/funccalc.py
@@ -0,0 +1,22 @@
+# a simple calculator with the help of defined functions
+
+
+# Sum function
+def sum(x, y):
+    return x + y
+
+#sub function
+def sub(x, y):
+    return x - y
+
+#div func
+def div(x, y):
+    return x / y
+
+#multiplication func
+def mul(x, y):
+    return x * y
+
+#the operators with the help of if else
+
+
