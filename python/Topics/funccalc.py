ff3bcef879980316c6ae484b5af8905affcb6ef3
Enziaki <jhashrijit@protonmail.com>
2021-02-17 21:22:47 +0530

    A def() calc

--- python/Topics/funccalc.py
+++ python/Topics/funccalc.py
@@ -3,20 +3,53 @@
 
 # Sum function
 def sum(x, y):
-    return x + y
+    sumr = return x + y
 
 #sub function
 def sub(x, y):
-    return x - y
+    subr = return x - y
 
 #div func
 def div(x, y):
-    return x / y
+    divr = return x / y
 
 #multiplication func
 def mul(x, y):
-    return x * y
+    multr = return x * y
+
+# absolute value
+def abss(x, y):
+    abssr = return str(abs(x)) + " and " + str(abs(y))
+
+
+#modulus value
+def mod(x, y):
+    modr = return x % y
+
+
 
 #the operators with the help of if else
 
+def op():
+    inp = input("Enter your operation here: [+, -, *, /, %, abs] \n")
+    if inp == '+':
+        num1 = input("The first number : ")
+        num2 = input("The second number : ")
+        def sum(num1, num2)
+        print(sumr)
+    elif inp == '-':
+        sub(num1, num2)
+        print(subr)
+    elif inp == '*':
+        mul(num1, num2)
+        print(multr)
+    elif inp == '/':
+        div(num1, num2)
+        print(divr)
+    elif inp == '%':
 
+
+
+
+
+ 
