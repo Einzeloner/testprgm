6e1a00b87cf67277ef51284c5501e71edda358a5
Enziaki <jhashrijit@protonmail.com>
2021-02-19 00:57:20 +0530

    An example of data type

---
+++ C/Topics/datatypes.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+	/* declaring some variables with different data types */
+	
+	int waifuno = 6;
+	/* place holder = %d */
+	
+	double loveforwaifus = 9.99;
+	/* place holder = %f */
+	
+	char rating = 'A';
+	/* place holder = %c */
+	
+	char waifuname[] = "Tohsaka, Touka, Miku, Asuka, Nagatoro, Ichika";
+	/* place holder = %s */
+	
+	
+	printf("I have %d number of waifus UwU\n", waifuno);
+	printf("From one to 10 I would rate them %f \n", loveforwaifus);
+	printf("All of them have %c rating for me UwU\n", rating);
+	printf("The names include- %s \n", waifuname);
+}
+
