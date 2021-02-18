fb3ed30c905f02480a7e133f47a62cc94856f740
Enziaki <jhashrijit@protonmail.com>
2021-02-19 00:19:54 +0530

    Added a new folder

---
+++ C/Topics/pp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	printf("       ___  \n");
+	printf("      /   \\ \n");
+	printf("     /     \\ \n");
+	printf("    /       \\ \n");
+	printf("   /         \\ \n");
+	printf("  /           \\\n");
+	printf(" /             \\\n");
+	printf(" _____      _____\n");
+	printf("     |      | \n");
+	printf("     |      |\n");
+	printf("     |      |\n");
+	printf("     |      | \n");
+	printf("  (((        )))\n");
+	printf("(((((        )))))\n");
+}
