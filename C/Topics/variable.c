c9d727c2a0e2af4c9ac9e91a70a73d2fc20533f3
Enziaki <jhashrijit@protonmail.com>
2021-02-19 00:38:35 +0530

    another variable type program

---
+++ C/Topics/variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	
+	char likings[] = "Onee sans";
+	char horni[] = "Ara Ara~";
+	int oneesansnumber = 5;
+	printf("I really really love %s\n", likings);
+	printf("Whenever they say %s I melt UwU\n", horni);
+	printf("The number of onee sans that I like is %d\n", oneesansnumber);
+}
+
