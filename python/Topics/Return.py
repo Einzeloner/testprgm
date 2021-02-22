1f49ec3dbae2a518e421fa78a3bd13826a04968c
Enziaki <jhashrijit@protonmail.com>
2021-02-22 16:17:45 +0530

    Return statement

---
+++ python/Topics/Return.py
@@ -0,0 +1,12 @@
+# The usage of return commands in python
+
+
+# usage of return commands with a defined function
+def mult(x):
+    return (x*x*x*x)/(x-1)
+    # will raise x to the power 4 and divide it with x-1 because why not
+
+randvar = mult(int(input("Enter a number to do some random calculations with: ")))
+
+# Now printing the random calc variable with the help of return
+print(randvar)
