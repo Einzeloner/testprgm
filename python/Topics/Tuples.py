737e97b5065ee67626b7a27dc282b30b59f62ed8
Enziaki <jhashrijit@protonmail.com>
2021-02-22 11:56:52 +0530

    Tuples in python

---
+++ python/Topics/Tuples.py
@@ -0,0 +1,11 @@
+# The difference between a list and a tuple is that a tuple can't be changed.
+# also that the tuple is defined with () instead of []
+
+t1 = (1, 2, 4,)
+print("A defined Tuple is " + str(t1))
+
+
+#printing an element from tuple
+print("Printing the second element of the tuple: " + str(t1[1]))
+# put shit in the sq brackets and the index that you put is the value that is 
+# going to be printed.
