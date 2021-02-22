4688c0d4f6e518de4ebb839348b424d94e2eb958
Enziaki <jhashrijit@protonmail.com>
2021-02-22 23:31:48 +0530

    moving into loops

---
+++ python/Topics/ifloops.py
@@ -0,0 +1,9 @@
+# A really simple example of if elese loops
+
+
+# using a user defined fucntion to explain if else
+def somefunc():
+    x = input("Would you like to create a list or a tuple?[l/t] ")
+    # now the if condition for if the statement has the response l
+    if  x == "l":
+        print("So you want to print a list")
