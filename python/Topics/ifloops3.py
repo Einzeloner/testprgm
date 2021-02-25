d4f39e4c150446f0249663a5fe5fbbd6a9cde04b
Enziaki <jhashrijit@protonmail.com>
2021-02-25 17:17:02 +0530

    and if loop

---
+++ python/Topics/ifloops3.py
@@ -0,0 +1,22 @@
+#if else with and as the condition
+# This feels like logic gates tbh
+
+# okay making an astolfo bot lmao
+
+# for the thing to wait
+import time
+
+# the male variable
+is_male = input("Are you a male? ")
+# the pp variable
+has_pp = input("Do you have a pp? ")
+# want some succ from astolfo
+succ = input("want some succ? ")
+
+# Astolfo bot starting uwu
+def astolfo():
+    print("Astolfo bot starting up...")
+    time.sleep(1)
+    print("knowing each other's name would make things smoother!")
+    time.sleep(0.5)
+    print("Boku wo namayewa Astolfo.\n I'm one of charlemagne's paladins.\n Now I am a rider class\n Pleased to meet you")
