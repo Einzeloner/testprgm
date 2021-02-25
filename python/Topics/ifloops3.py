c6eeaa33682cb4cea05356d1d52c982091c595e6
Enziaki <jhashrijit@protonmail.com>
2021-02-26 04:45:49 +0530

    A problematic code

--- python/Topics/ifloops3.py
+++ python/Topics/ifloops3.py
@@ -11,12 +11,39 @@ is_male = input("Are you a male? ")
 # the pp variable
 has_pp = input("Do you have a pp? ")
 # want some succ from astolfo
-succ = input("want some succ? ")
+succ = bool(input("want some succ? "))
 
 # Astolfo bot starting uwu
 def astolfo():
     print("Astolfo bot starting up...")
+    # Sleep boi astolfo uwu
     time.sleep(1)
     print("knowing each other's name would make things smoother!")
     time.sleep(0.5)
     print("Boku wo namayewa Astolfo.\n I'm one of charlemagne's paladins.\n Now I am a rider class\n Pleased to meet you")
+    # user name input
+    ast_name = input("And what is your name? ")
+    
+    # Gender distinction lmao
+    if is_male == 'yes' or is_male == 'Yes' or is_male == 'y' or is_male == 'Y':
+        print("OWO Nice to meet you %s kun!" %ast_name)
+        time.sleep(2)
+    elif  is_male == 'No' or is_male == 'no' or is_male == 'n' or is_male == 'N':
+        print("OWO Nice to meet you %s Chan uwu!" %ast_name)
+        time.sleep(2)
+    # If they are the neither
+    else:
+        pronoun = input("Uhh.. Ano.. what  should I call you Anata~: ")
+        print("Yoshiii I will call you %s Chan Anataa" %pronoun)
+        fine = input("Is that fine anataa... ")
+        if fine == 'yes' or fine == 'y' or fine == 'Y' or y == 'Yes':
+            print("yattaaa! I will call you %s chan", &pronoun)
+        else:
+            print("Baka I will exit now")
+
+
+    # I seriously don't know what to do
+    if  
+        if fine == 'yes' or fine == 'y' or fine == 'Y' or y == 'Yes':
+    
+
