728e4d827b4359194d081ab99aa11f15eaa045a5
Enziaki <jhashrijit@protonmail.com>
2021-03-02 15:45:07 +0530

    While loops

---
+++ python/Topics/while.py
@@ -0,0 +1,18 @@
+# About while loops
+
+# printing a list of numbers one by one in new line 
+
+x  = 1
+
+z = int(input("Enter the number to be printed till in the series: "))
+while x <= z:
+    print(x)
+    x += 1
+
+
+print("No more")
+
+# The while loop works in this way;
+# the variable z is checked for condition specified with relation to x
+# It constantly checks everything in the condition until the condition is false one
+# After that it motherfucking stops
