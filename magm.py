b60be3e0e7a6ea8f634358c38a9f32695845dc98
Enziaki <jhashrijit@protonmail.com>
2021-03-14 22:57:02 +0530

    Finishing the guessing game

--- magm.py
+++ magm.py
@@ -1,9 +1,24 @@
 # Maybe a guessing game
 
 secword = "whatever"
+# an empty guess variable 
 guess = ""
+# The count of the guesses that a user has made 
+guess_count = 0
+# The limit for the guesses 
+guess_limit = 3
 
+# While the condition in the while loop matches,  run the code again and
 while guess != secword:
-    guess = input("Enter the secret word: ")
-
-print("bruh you win lmao")
+    if guess_count < 3:
+        guess = input("Enter guess: ") 
+        guess_count += 1
+    else:
+        print("Out of guesses!")
+        break
+    if guess_count == 3:
+        print("You lose!")
+        break
+    if  guess == secword:
+        print("You win!")
+# print("You win")
