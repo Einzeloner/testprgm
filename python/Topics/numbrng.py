778eda712e920421b4c268e6926e326504d00fdb
Enziaki <jhashrijit@protonmail.com>
2021-03-31 03:46:01 +0530

    a simple for loop example done

---
+++ python/Topics/numbrng.py
@@ -0,0 +1,5 @@
+# python program to print a series of elements
+x = int(input("Enter an integer number here baka >_< "))
+# A for loop to iterate over the function and apply until the condition is met
+for i in range(x + 1):
+    print(i * '*')
