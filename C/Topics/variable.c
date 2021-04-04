5f8ed43adb6da1b37c2efbd21b595f40747ab3f8
Enziaki <jhashrijit@protonmail.com>
2021-04-04 05:44:46 +0530

    Added some comments

--- C/Topics/variable.c
+++ C/Topics/variable.c
@@ -4,8 +4,21 @@ int main(){
 	char likings[] = "Onee sans";
 	char horni[] = "Ara Ara~";
 	int oneesansnumber = 5;
+	
+	
+	// Storing more variables 
+
+	// Storing an integer value in a variable
+	int num = 8;
+
+	// Storing a floating value
+	float numdec = 65.66;
+	// Storing a string with characters
+	char fuck[] = "whatevr the fuck";
+
 	printf("I really really love %s\n", likings);
-	printf("Whenever they say %s I melt UwU\n", horni);
+	printf("Whenever they say %s I melt UwU\n", fuck);
+	printf("And a floating point number would be %0.2f\n", numdec);
 	printf("The number of onee sans that I like is %d\n", oneesansnumber);
 }
 
