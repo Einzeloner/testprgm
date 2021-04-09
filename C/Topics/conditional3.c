03e16c16780f6a7447f7349c0cf8cf69d053632d
Enziaki <jhashrijit@protonmail.com>
2021-04-09 08:07:00 +0530

    Another if else program

--- C/Topics/conditional3.c
+++ C/Topics/conditional3.c
@@ -1,2 +1,35 @@
 // Another condtional program to check and display income tax
 
+/* condition given
+       income slab                Tax
+        2.5l - 5.0l                     5%
+        5.0l - 10.0l                  20%
+        Above 10l                    30%
+        no tax below 2.5l
+*/
+
+# include <stdio.h>
+
+void main(){
+    float  income;
+
+   printf("Enter your annual income: ");
+   scanf("%f", &income);
+    
+    if (income <= 250000){
+        printf("Your income tax will be: 0\n");
+    }
+    else if (income >= 250000 && income <= 500000){
+        printf("Your income tax would be: %0.2f\n", income * 0.05);
+    }
+    else if (income >= 500000 && income <= 1000000){
+        printf("Your income tax would be: %0.2f\n", income * 0.2);
+    }
+    else if(income >= 1000000){
+         printf("Your income tax will be: %0.2f\n", income * 0.3);
+    }
+    else{
+        printf("Invalid input\n");
+    }
+
+}
