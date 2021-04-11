8dcf76ea5aec1128bceea3be9c9b27563287b898
Enziaki <jhashrijit@protonmail.com>
2021-04-11 17:00:16 +0530

    A simple while loop prog

---
+++ C/Topics/loops.c
@@ -0,0 +1,16 @@
+// A program to understand loops 
+# include <stdio.h>
+
+void main(){
+    int num = 0;
+    
+    /* while loop-  will execute the instructions in the loop till the condition in the 
+     parenthesis is true
+     */
+    while( num <= 20){
+        printf("%d\n", num);
+        // Increment operator, adds +1 with every iteration
+        num++;
+
+    }
+}
