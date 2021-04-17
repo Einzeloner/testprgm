f218eb6da0f8f8b2b21fbea6e54fafff3ab7734d
Enziaki <jhashrijit@protonmail.com>
2021-04-17 14:36:15 +0530

    Finishing for loops

--- C/Topics/for.c
+++ C/Topics/for.c
@@ -7,11 +7,8 @@ void main(){
 
     printf("Enter the number of increments: ");
     scanf ("%d", &userinput);
-
-    // Initialize the variable , condition and instruction in the for loop all at once.
-
-    for (int a=0; a <= userinput; a++){
-        printf("Incrementation: %d\n", a);
-    
-    }
+      
+      for (int i = userinput; i; --i){
+          printf("%d\n", i);
+      }
 }
