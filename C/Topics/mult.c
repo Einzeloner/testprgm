3e3b8f0d8ba8388ab88bb0ddec1ccd2ef1f56522
Enziaki <jhashrijit@protonmail.com>
2021-04-22 03:21:39 +0530

    A bit more modified mult program

--- C/Topics/mult.c
+++ C/Topics/mult.c
@@ -11,5 +11,8 @@
     for (int a = 1; a <= 10; a++ ){
         printf("%d\n",  fuckingnumber * a);
 
-    }     
+    } 
+    for (int a = 10; a >= 1; a--){
+        printf("In reverse order: %d\n", fuckingnumber * a);
+    }    
  }
