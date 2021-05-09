0d125f5c979a58c0a0f0d395987ba483bc957c67
Enziaki <jhashrijit@protonmail.com>
2021-05-09 22:01:23 +0530

    A random wip prog

---
+++ C/test/shit.c
@@ -0,0 +1,10 @@
+// A random C  program for heirarchy things
+
+# include <stdio.h>
+
+int  main() {
+    int fuck = 5 *2 *2 * 5 - 3 * 3 * 4 * 4 - 8 * 2 * 2 * 5 + 10 * 3 * 4;
+    
+    printf("%d", fuck);
+
+}
